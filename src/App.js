22c572847ec4b5182b457c3484d0a13d5a3c6f7f
Sebe Keller <sebethkeller@gmail.com>
2017-04-11 17:25:59 -0400

    Hello World up and running

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HelloWorld from './HelloWorld';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <HelloWorld/>
+      <HelloWorld/>
+    </div>
+  );
+};
+
+export default App;
